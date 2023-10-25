ebeca60d79d758c13ebe74b201f0aa30292ecfd7
treerspeaking <93274119+treerspeaking@users.noreply.github.com>
2023-10-25 15:37:38 +0700

    "complete exercise

--- StudentController.java
+++ StudentController.java
@@ -1,3 +1,30 @@
 public class StudentController{
-	
+	// constructor for StudentController
+  private Student student;
+  private StudentView studentView;
+  public StudentController(Student student, StudentView studentView){
+    this.student = student;
+    this.studentView = studentView;    
+  }
+
+  public void setStudentName(String name){
+    student.setName(name);
+  }
+
+  public String getStudentName(){
+    return student.getName();
+  }
+
+  public void setStudentRollNo(String rollNo){
+    student.setRollNo(rollNo);
+  }
+
+  public String getStudentRollNo(){
+    return student.getRollNo();
+  }
+
+  public void updateView(){
+    studentView.printStudentDetails(student);
+  }
+
 }
--- StudentView.java
+++ StudentView.java
@@ -1,3 +1,7 @@
 public class StudentView{
-	
+	public void printStudentDetails(Student student){
+    System.out.println("Student: ");
+    System.out.println("Name: " + student.getName());
+    System.out.println("Roll No: " + student.getRollNo());
+  }
 }
